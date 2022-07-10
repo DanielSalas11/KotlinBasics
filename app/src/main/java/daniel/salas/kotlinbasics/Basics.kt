8392ffe25a0e051e1be7428f208e05aa7ff40bc7
DanielSalas11 <daniel_salas11@outlook.com>
2022-07-09 23:50:07 -0700

    Introduction to data types in Kotlin, quick test

--- app/src/main/java/daniel/salas/kotlinbasics/Basics.kt
+++ app/src/main/java/daniel/salas/kotlinbasics/Basics.kt
@@ -7,5 +7,21 @@ fun main(){
     val myName = "Daniel"
     var myAge = 22
     //myName = "newDaniel"
-    print("Hi my name is " + myName + ".")
+    print("Hi my name is " + myName + "\n")
+
+    val myString = "RandomString"
+    var randomChar = myString[0]
+    var lastChar = myString[myString.length -1]
+    println(randomChar)
+    println(lastChar)
+
+    //Assigning values to their specific data type exercise:
+    var courseName: String= "Android Masterclass"
+    val thirtheenNumber: Float = 13.37F
+    var twentyFive: Byte = 25
+    var year: Short = 2020
+    var longNumber: Long = 18881234567
+    var buleano: Boolean = true
+    var caracter: Char= 'a'
+
 }
