4e27e651cdd157daf7d15294c7d9eeb8f48e7ad6
DanielSalas11 <daniel_salas11@outlook.com>
2022-07-09 17:17:38 -0700

    Testing types of data

--- app/src/main/java/daniel/salas/kotlinbasics/Basics.kt
+++ app/src/main/java/daniel/salas/kotlinbasics/Basics.kt
@@ -1,7 +1,11 @@
 package daniel.salas.kotlinbasics
 
 fun main(){
+    //immutable variable
+    // TODO: Add new functionality
+
     val myName = "Daniel"
+    var myAge = 22
     //myName = "newDaniel"
     print("Hi my name is " + myName + ".")
 }
